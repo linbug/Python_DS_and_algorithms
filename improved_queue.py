77eb851d4e381ee5f737b3757124fabe0c47ef58
Lin Taylor <taylorxlin@gmail.com>
2016-02-25 13:19:49 +0000

    Adds queues

---
+++ improved_queue.py
@@ -0,0 +1,29 @@
+class ImprovedQueue:
+    def __init__(self):
+        self.length = 0
+        self.head = None
+        self.last = None
+
+    def is_empty(self):
+        return self.length == 0
+
+    def insert(self, cargo):
+        node = Node(cargo)
+        if self.length == 0:
+            # If list is empty, the new node is head and last
+            self.head = self.last = node
+        else:
+            # Find the last node
+            last = self.last
+            # Append the new node
+            last.next = node
+            self.last = node
+        self.length += 1
+
+    def remove(self):
+        cargo = self.head.cargo
+        self.head = self.head.next
+        self.length -= 1
+        if self.length == 0:
+            self.last = None
+        return cargo
