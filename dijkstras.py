79527c6d7e0c7c5d4342050baf22cdddeaeeaf54
Lin Taylor <taylorxlin@gmail.com>
2016-02-26 17:06:17 +0000

    algorithm now works properly

--- dijkstras.py
+++ dijkstras.py
@@ -4,36 +4,47 @@ costs = {'start':0, 'A':6, 'B':2, 'fin' : float('inf')}
 
 graph = {'start': {'A':6, 'B':2},'A':{'fin':1},'B':{'A':3,'fin':5}, 'fin':{}}
 
-parents = {'A':'start', 'B':'start'}
+parents = {'start':None, 'A':'start', 'B':'start'}
 
 #Workflow:
 #1. find the cheapest node
-#2. check f there is a cheaper path to the neighbours of this node
+#2. check if there is a cheaper path to the neighbours of this node. If so, update node costs and parent.
 #3. repeat until you have done this for every node in the graph
 #4. calculate the final path
 
-# current_node = 'start'
-# queue = ['A','B']
-#if there's still something left in the queue, look for any child nodes; add these to the queue if they've not been added before
-#for children of current node, if cost < (costs[current_node] + costs[child], update costs to graph['current_node']['child'], update parents
-#repeat until there's nothing left in the queue
-#calculate the distance starting from the end point and moving backwards along the parents dictionary
-
 def dijkstra(current_node = 'start'):
-    queue = list(graph[current_node])
-    seen = [current_node]+list(graph[current_node])
-    while queue:
-        current_node = queue.pop(0)
+    '''runs dijkstra's algorithm'''
+    processed = [current_node]                                  #keep track of nodes that have already been processed
+    current_node = find_lowest_cost_node(costs, processed)      #find the lowest cost unprocessed node
+    while current_node:                                         #while there are unprocessed nodes left
         print('current_node='+str(current_node))
-        for i in list(graph[current_node]): #add children to the queue if they haven't previously been added
-            if i not in seen:
-                queue.append(i)
-                seen.append(i)
-                print('adding '+ str(list(graph[current_node])) + ' to the queue')
-                print(seen)
-        for child in list(graph[current_node]): #calculate whether there is a cheaper way to reach the children of the current node
-            if costs[child]<(costs[current_node]+costs[child]):
-                costs[child] = graph[current_node][child] + costs[current_node]
-                print('updating cost of ' + str(child) + ' to ' + str(graph[current_node][child] + costs[current_node]))
-                parents[child] = current_node
+        for child in list(graph[current_node]):                 #calculate whether there is a cheaper way to reach the children of the current node
+            new_cost = costs[current_node] + graph[current_node][child]
+            if costs[child]>new_cost:
+                costs[child] = new_cost                         #if so, update the cost of that child
+                print('updating cost of ' + str(child) + ' to ' + str(new_cost))
+                parents[child] = current_node                   #and update the parent
                 print('updating parent of ' + str(child) + ' to ' + str(current_node))
+        processed.append(current_node)
+        current_node = find_lowest_cost_node(costs, processed)
+    return find_final_path(parents)
+
+def find_lowest_cost_node(costs, processed):
+    '''find the lowest cost node of the unprocessed nodes'''
+    lowest_cost = float('inf')
+    lowest_node = None
+    for node in list(costs.keys()):
+        if costs[node] < lowest_cost and node not in processed:
+            lowest_cost = costs[node]
+            lowest_node = node
+    return lowest_node
+
+def find_final_path(parents):
+    '''return the path between start and fin'''
+    current_node = parents['fin']
+    path = "-->fin"
+    while current_node:
+        path = '-->' + path
+        path = str(current_node) + path
+        current_node = parents[current_node]
+    return path
